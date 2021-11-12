3bb7120bd6ee0ee5ae63f3b1f3ee2f77b2d08286
Nico <nicolorenzo@live.com>
2021-11-12 00:15:25 -0300

    checkpoint

--- Programacao-Imperativa/Aula-10/app.js
+++ Programacao-Imperativa/Aula-10/app.js
@@ -1,28 +1,28 @@
 function microondas(prato, tempo){
     switch(prato){
         case 'pipoca':
-            a(tempo,10);
+            mensagem(tempo,10);
             break;
         case 'macarrao':
-            a(tempo,18)
+            mensagem(tempo,18)
              break;
         case 'carne':
-            a(tempo,15)
+            mensagem(tempo,15)
             break;
         case 'feijao':
-            a(tempo,12)
+            mensagem(tempo,12)
             break;
         case 'brigadeiro':
-            a(tempo,8)
+            mensagem(tempo,8)
             break;
         default:
             console.log('prato inexiste');
     }
-    function a(tempo,tempoIdeal){
+    function mensagem(tempo,tempoIdeal){
         tempo<tempoIdeal? console.log("tempo insuficiente"):
         tempo>=tempoIdeal*2? console.log("comida queimou"):
         tempo>=tempoIdeal*3? console.log("kabumm"):
         console.log("Prato pronto, bom apetite!!!")
     }
 }
-microondas('pizaz',9)
+microondas('carne',9)
