29a5f0d79ca48e1691912f8730be16509849f259
Nico <nicolorenzo@live.com>
2021-12-02 14:12:33 -0300

    Aula 15 - módulos

---
+++ Programacao-Imperativa/Aula-15/app.js
@@ -0,0 +1,3 @@
+const grupo = require('./modulos')
+grupo.maiorAlturaMulheres();
+grupo.menorAlturaMulheres();
---
+++ Programacao-Imperativa/Aula-15/modulos/index.js
@@ -0,0 +1,61 @@
+function Pessoa(nome, altura, sexo) {
+    this.nome = nome;
+    this.altura = altura;
+    this.sexo = sexo;
+}
+
+let pessoa1 = new Pessoa('Nico', 1.69, 'M');
+let pessoa2 = new Pessoa('Rosa', 1.6, 'F');
+let pessoa3 = new Pessoa('Dilma', 1.8, 'F');
+
+module.exports = {
+    grupo: [pessoa1, pessoa2, pessoa3],
+    novaPessoa: function(nome, altura, sexo) {
+        this.nome = nome;
+        this.altura = altura;
+        this.sexo = sexo;
+    },
+    mediaAlturaMulheres: function() {
+        let somaMulheres = 0;
+        let somaAltura = 0;
+        for(let i =0; i<this.grupo.length; i++) {
+            if(this.grupo[i].sexo == 'F'){
+                somaMulheres++;
+                somaAltura += this.grupo[i].altura;
+            }
+        }
+        return 'A média de altura entre as mulheres é de: '+somaAltura/somaMulheres;
+    },
+    maiorAlturaMulheres: function() {
+        let maiorAltura = 0;
+        let maiorMulher=0;
+        for(let i =0; i<this.grupo.length; i++) {
+            if(this.grupo[i].sexo == 'F'){
+                if(this.grupo[i].altura > maiorAltura){
+                    maiorMulher = this.grupo[i].nome;
+                    maiorAltura = this.grupo[i].altura
+                }
+            }
+        }
+        return console.log('A mulher mais alta é '+maiorMulher+ ' que tem '+maiorAltura+' metros');
+    },
+    menorAlturaMulheres: function() {
+        let menorAltura = 0;
+        let menorMulher = 0;
+        for(let i =0; i<this.grupo.length; i++) {
+            if(this.grupo[i].sexo == 'F'){
+                menorAltura = this.grupo[i].altura;
+                for(let i =0; i<this.grupo.length; i++) {
+                    if(this.grupo[i].altura < menorAltura){
+                        menorMulher = this.grupo[i].nome;
+                        menorAltura = this.grupo[i].altura
+                    }
+        
+                }
+            }
+        }
+
+        return console.log('A mulher mais alta é '+menorMulher+ ' que tem '+menorAltura+' metros');
+    }
+
+}
